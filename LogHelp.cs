568128508ffdd0900797c8c42e3c31b6f60ef201
tianqiq <tianqiq@gmail.com>
2013-04-06 23:44:28 +0800

    log

---
+++ LogHelp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Web;
+
+namespace StaticHtml
+{
+    static class LogHelp
+    {
+
+        readonly static TraceSwitch ts;
+
+        static LogHelp()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(HttpRuntime.AppDomainAppPath + "staticHtml_log.txt"));
+            Trace.AutoFlush = true;
+            ts = new TraceSwitch("staticHtml", "staticHtml 日志开关", "2");
+        }
+
+        public static void Write(string message)
+        {
+            Trace.WriteLine(message);
+        }
+
+        public static void Info(string message)
+        {
+            if (ts.TraceInfo)
+            {
+                Trace.TraceInformation(message);
+            }
+        }
+
+        public static void Warn(string message)
+        {
+            if (ts.TraceWarning)
+            {
+                Trace.TraceInformation(message);
+            }
+        }
+
+        public static void Error(string message)
+        {
+            if (ts.TraceError)
+            {
+                Trace.TraceInformation(message);
+            }
+        }
+
+    }
+}
